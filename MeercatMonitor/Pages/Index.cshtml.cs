4c1dee20c7785fb811e4f6db3fba14f740e7c802
Jan Klass <kissaki@posteo.de>
2025-03-08 16:50:41 +0100

    feat: Introduce group names

--- MeercatMonitor/Pages/Index.cshtml.cs
+++ MeercatMonitor/Pages/Index.cshtml.cs
@@ -2,13 +2,26 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MeercatMonitor.Pages
 {
-    internal class IndexPageModel(OnlineStatusStore _store) : PageModel
+    internal class IndexPageModel(Config _config, OnlineStatusStore _store) : PageModel
     {
-        public IReadOnlyCollection<OnlineStatusStore.Result> AllStatus { get; private set; } = [];
+        public record GroupResultsModel(MonitorGroup Group, OnlineStatusStore.Result?[] Results);
+
+        public GroupResultsModel[] GroupResults { get; private set; } = [];
 
         public void OnGet()
         {
-            AllStatus = _store.GetAll();
+            GroupResults = _config.Monitors.Select(GetGroupResults).ToArray();
+
+            GroupResultsModel GetGroupResults(MonitorGroup group)
+            {
+                OnlineStatusStore.Result?[] results = group.Addresses.Select(GetTargetStatus).ToArray();
+                return new(group, results);
+            }
+
+            OnlineStatusStore.Result? GetTargetStatus(ToMonitorAddress target)
+            {
+                return _store.TryGetValue(target, out var result) ? result : null;
+            }
         }
     }
 }
