95ba4de2f743b3ad9d7405f27bbbbe06c5a79ec7
Jan Klass <kissaki@posteo.de>
2025-03-08 16:23:24 +0100

    refac(page): Load model data on get instead of get on render

--- MeercatMonitor/Pages/Index.cshtml.cs
+++ MeercatMonitor/Pages/Index.cshtml.cs
@@ -1,12 +1,14 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MeercatMonitor.Pages
 {
-    public class IndexModel : PageModel
+    internal class IndexPageModel(OnlineStatusStore _store) : PageModel
     {
+        public IReadOnlyCollection<OnlineStatusStore.Result> AllStatus { get; private set; } = [];
+
         public void OnGet()
         {
+            AllStatus = _store.GetAll();
         }
     }
 }
