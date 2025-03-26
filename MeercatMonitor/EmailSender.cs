Frog-Gaming/meercat-monitor

commit 12f9d26211a91c8fa52c2e0e1de1e59c4695d200

test: Introduce `--test` and injected TestConfig

MeercatMonitor/EmailSender.cs
@@ -4,7 +4,7 @@ using MimeKit;
 
 namespace MeercatMonitor;
 
-internal class EmailSender(Config _config)
+internal class EmailSender(Config _config, TestConfig? _testConfig)
 {
     public void SendFor(ToMonitorAddress toMonitorAddress, bool isOnline)
     {
@@ -14,6 +14,8 @@ internal class EmailSender(Config _config)
             var recipients = monitorGroup.Recipients.Distinct().ToArray();
             var message = CreateMessage(_config.Sender, recipients);
             SetMessageText(message, toMonitorAddress, isOnline, monitorGroup.Texts);
+
+            if (!(_testConfig?.SendEmails ?? true)) return;
 
             Send(message, _config);
         }

MeercatMonitor/OnlineMonitor.cs
@@ -2,7 +2,7 @@ using System.Net.Sockets;
 
 namespace MeercatMonitor;
 
-internal class OnlineMonitor(Config config, NotificationService _notify, ILogger<OnlineMonitor> _log, OnlineStatusStore _statusStore) : BackgroundService
+internal class OnlineMonitor(Config config, NotificationService _notify, ILogger<OnlineMonitor> _log, OnlineStatusStore _statusStore, TestConfig? _testConfig) : BackgroundService
 {
     private readonly PeriodicTimer _timer = new(TimeSpan.FromSeconds(config.CheckIntervalS));
     // Distinct() across groups and also work around duplicate config list values
@@ -116,6 +116,8 @@ internal class OnlineMonitor(Config config, NotificationService _notify, ILogger
         _log.LogInformation("Starting monitoring of ({MonitorTargetCount}) [{MonitorTargets}]…", _toMonitorAddresses.Length, string.Join(", ", _toMonitorAddresses.Select(x => $"{x.Name} ({x.Address})")));
         do
         {
+            if (!(_testConfig?.SendMonitorRequests ?? true)) continue;
+
             _log.LogInformation("Checking {MonitorTargetCount} targets…", _toMonitorAddresses.Length);
             foreach (var websiteAddress in _toMonitorAddresses)
             {

MeercatMonitor/OnlineStatusStore.cs
@@ -1,6 +1,6 @@
 namespace MeercatMonitor;
 
-public class OnlineStatusStore
+public class OnlineStatusStore(TestConfig? _testConfig)
 {
     public enum Status { Unknown, Online, Offline, }
 
@@ -10,7 +10,19 @@ public class OnlineStatusStore
 
     private readonly Dictionary<ToMonitorAddress, List<Result>> _store = [];
 
-    public IEnumerable<Result> GetValues(ToMonitorAddress key) => _store.TryGetValue(key, out var results) ? [.. results] : [];
+    public IEnumerable<Result> GetValues(ToMonitorAddress key)
+    {
+        if (_testConfig?.FillTestData is not null && !_store.ContainsKey(key))
+        {
+            for (var i = _testConfig.FillTestData.Value; i > 0; i--)
+            {
+                var dto = DateTimeOffset.Now.AddMinutes(-10 * i);
+                Push(key, new(Status.Online, dto));
+            }
+        }
+
+        return _store.TryGetValue(key, out var results) ? [.. results] : [];
+    }
 
     public void SetNow(ToMonitorAddress key, Status status) => Push(key, new Result(status, DateTimeOffset.Now));
 

MeercatMonitor/Program.cs
@@ -22,6 +22,11 @@ builder.Services.AddSerilog(ConfigureLogger);
 builder.Services.AddSingleton(config);
 builder.Services.AddRazorPages();
 
+if (args.Contains("--test"))
+{
+    builder.Services.AddSingleton(new TestConfig(SendMonitorRequests: false, SendEmails: true, FillTestData: 100));
+}
+
 var host = builder.Build();
 host.UseStaticFiles();
 host.UseRouting();

MeercatMonitor/Config.cs
@@ -10,3 +10,5 @@ public record MailAddress(string Name, string Address);
 public record MailServer(string Address, int Port, bool IgnoreCertValidation = true);
 
 public record Texts(string SubjWentOnline, string SubjWentOffline, string? BodyPlainWentOnline = null, string? BodyPlainWentOffline = null, string? BodyHtmlWentOnline = null, string? BodyHtmlWentOffline = null);
+
+public record TestConfig(bool SendMonitorRequests, bool SendEmails, int? FillTestData);

commit 08c8d28c910385497d597265bcf922e8d73831bb

fix: Fix normal startup regression regarding TestConfig
The regression was introduced in 12f9d26211a91c8fa52c2e0e1de1e59c4695d200

MeercatMonitor/EmailSender.cs
@@ -4,7 +4,7 @@ using MimeKit;
 
 namespace MeercatMonitor;
 
-internal class EmailSender(Config _config, TestConfig? _testConfig)
+internal class EmailSender(Config _config, TestConfig _testConfig)
 {
     public void SendFor(ToMonitorAddress toMonitorAddress, bool isOnline)
     {

MeercatMonitor/OnlineMonitor.cs
@@ -2,7 +2,7 @@ using System.Net.Sockets;
 
 namespace MeercatMonitor;
 
-internal class OnlineMonitor(Config config, NotificationService _notify, ILogger<OnlineMonitor> _log, OnlineStatusStore _statusStore, TestConfig? _testConfig) : BackgroundService
+internal class OnlineMonitor(Config config, NotificationService _notify, ILogger<OnlineMonitor> _log, OnlineStatusStore _statusStore, TestConfig _testConfig) : BackgroundService
 {
     private readonly PeriodicTimer _timer = new(TimeSpan.FromSeconds(config.CheckIntervalS));
     // Distinct() across groups and also work around duplicate config list values

MeercatMonitor/OnlineStatusStore.cs
@@ -1,6 +1,6 @@
 namespace MeercatMonitor;
 
-public class OnlineStatusStore(TestConfig? _testConfig)
+public class OnlineStatusStore(TestConfig _testConfig)
 {
     public enum Status { Unknown, Online, Offline, }
 

MeercatMonitor/Program.cs
@@ -22,10 +22,8 @@ builder.Services.AddSerilog(ConfigureLogger);
 builder.Services.AddSingleton(config);
 builder.Services.AddRazorPages();
 
-if (args.Contains("--test"))
-{
-    builder.Services.AddSingleton(new TestConfig(SendMonitorRequests: false, SendEmails: true, FillTestData: 100));
-}
+var isTest = args.Contains("--test");
+builder.Services.AddSingleton(new TestConfig(SendMonitorRequests: !isTest, SendEmails: true, FillTestData: isTest ? 100 : 0));
 
 var host = builder.Build();
 host.UseStaticFiles();