1975846d3fb43912febfc997039c30e979541d3a
Jan Klass <kissaki@posteo.de>
2025-07-15 22:00:34 +0200

    feat: Introduce second-chance mechanism (HTTP) BREAKING
    Requires config new value.

--- MeercatMonitor/Checkers/HttpChecker.cs
+++ MeercatMonitor/Checkers/HttpChecker.cs
@@ -3,13 +3,32 @@ using System.Net;
 
 namespace MeercatMonitor.Checkers;
 
-public class HttpChecker(ILogger<HttpChecker> _log, StatusUpdater _statusUpdater, IHttpClientFactory _httpFact) : IChecker
+public class HttpChecker(ILogger<HttpChecker> _log, StatusUpdater _statusUpdater, IHttpClientFactory _httpFact, Config _config) : IChecker
 {
     public TimeSpan Timeout { get; } = TimeSpan.FromSeconds(10);
 
     public bool Supports(MonitorTarget target) => target.Address.Scheme is "http" or "https";
 
     public async Task CheckAsync(MonitorTarget target)
+    {
+        var result = await CheckAsyncImpl(target);
+        // Unhandled unexpected program behavior; warning expected already logged => no further actions
+        if (result is null) return;
+
+        if (!result.IsOnline && _config.Retry.SecondChance)
+        {
+            _log.LogInformation("Determined offline. Starting second-chance check…");
+            result = await CheckAsyncImpl(target);
+            // Unhandled unexpected program behavior; warning expected already logged => no further actions
+            if (result is null) return;
+        }
+
+        _statusUpdater.UpdateStatus(result.Target, result.IsOnline, result.Elapsed, result.ResponseDetails);
+    }
+
+    private sealed record Result(MonitorTarget Target, bool IsOnline, TimeSpan Elapsed, string ResponseDetails);
+
+    private async Task<Result?> CheckAsyncImpl(MonitorTarget target)
     {
         _log.LogDebug("Checking {TargetAddress}…", target.Address);
 
@@ -23,24 +42,27 @@ public class HttpChecker(ILogger<HttpChecker> _log, StatusUpdater _statusUpdater
             HttpResponseMessage res = await c.SendAsync(req);
 
             var isOnline = res.IsSuccessStatusCode;
-
-            _statusUpdater.UpdateStatus(target, isOnline, sw.Elapsed, FormatStatusCode(res.StatusCode));
+            var responseDetails = FormatStatusCode(res.StatusCode);
+            return new(target, isOnline, sw.Elapsed, responseDetails);
         }
         catch (HttpRequestException ex)
         {
             _log.LogWarning(ex, "HTTP {TargetAddress} failed the uptime check with exception {ExceptionMessage}", target.Address, ex.Message + ex.InnerException?.Message);
-
-            _statusUpdater.UpdateStatus(target, isOnline: false, sw.Elapsed, ex.StatusCode is not null ? FormatStatusCode(ex.StatusCode.Value) : $"{ex.Message}; {ex.InnerException?.Message}");
+            var isOnline = false;
+            var responseDetails = ex.StatusCode is not null ? FormatStatusCode(ex.StatusCode.Value) : $"{ex.Message}; {ex.InnerException?.Message}";
+            return new(target, isOnline, sw.Elapsed, responseDetails);
         }
         catch (TaskCanceledException ex)
         {
             _log.LogWarning(ex, "HTTP {TargetAddress} failed the uptime check with exception {ExceptionMessage}", target.Address, ex.Message + ex.InnerException?.Message);
-
-            _statusUpdater.UpdateStatus(target, isOnline: false, sw.Elapsed, "⏱ Timeout");
+            var isOnline = false;
+            var responseDetails = "⏱ Timeout";
+            return new(target, isOnline, sw.Elapsed, responseDetails);
         }
         catch (Exception ex)
         {
             _log.LogWarning(ex, "HTTP {TargetAddress} failed the uptime check with exception {ExceptionMessage}", target.Address, ex.Message + ex.InnerException?.Message);
+            return null;
         }
     }
 
--- MeercatMonitor/Config.cs
+++ MeercatMonitor/Config.cs
@@ -1,6 +1,6 @@
 namespace MeercatMonitor;
 
-public record Config(int CheckIntervalS, int? CheckTimeoutS, int? HistoryDisplayLimit, MonitorGroup[] Monitors, MailAddress Sender, MailServer MailServer);
+public record Config(int CheckIntervalS, int? CheckTimeoutS, int? HistoryDisplayLimit, Retry Retry, MonitorGroup[] Monitors, MailAddress Sender, MailServer MailServer);
 
 public record MonitorGroup(string Name, string Slug, MonitorTarget[] Targets, MailAddress[] Recipients, Texts Texts);
 
@@ -12,3 +12,5 @@ public record MailServer(string Address, int Port, bool IgnoreCertValidation = t
 public record Texts(string SubjWentOnline, string SubjWentOffline, string? BodyPlainWentOnline = null, string? BodyPlainWentOffline = null, string? BodyHtmlWentOnline = null, string? BodyHtmlWentOffline = null);
 
 public record TestConfig(bool SendMonitorRequests, bool SendEmails, int? FillTestData);
+
+public record Retry(bool SecondChance);
